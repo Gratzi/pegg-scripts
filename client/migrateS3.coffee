410fa5d62cc3b366fabe9b62f1e1abed2ae461a7
Auggernaut <augustin@datacosmos.com>
2014-09-19 16:47:22 -0700

    add moveImageToS3. Fails because of Parse bullshit, need to move to server.

--- client/migrateS3.coffee
+++ client/migrateS3.coffee
@@ -3,6 +3,15 @@ filepicker.setKey "A36NnDQaISmXZ8IOmKGEQz"
 
 class Migrate
 
+  moveImagesToS3: ->
+    @fetchImageUrls 5, 0, [], (items) ->
+      for item in items
+        matches = item.url.match /[^\/]+(#|\?|$)/
+        filename = "_#{item.id}.#{matches[0]}"
+        path = '/full'
+#        @saveImageToS3 item.url, filename, path, (inkBlob) =>
+        console.log item
+
   pickAndStore: ->
     filepicker.pickAndStore mimetype: "image/*", {path: '/uploaded/'}, (InkBlobs) ->
       result = InkBlobs[0]
@@ -12,20 +21,42 @@ class Migrate
         result.thumb = thumbBlob
 
 
-  fetchImageUrls: ->
+  fetchImageUrls: (limit, skip, urls, cb) ->
     Choice = Parse.Object.extend 'Choice'
     choiceQuery = new Parse.Query Choice
-    card = new Parse.Object 'Card'
-  #  card.set 'id',  cardId
-  #  choiceQuery.equalTo 'card', card
+#    card = new Parse.Object 'Card'
+#    card.set 'id',  cardId
+#    choiceQuery.equalTo 'card', card
+    choiceQuery.limit limit
+    choiceQuery.skip skip
     choiceQuery.find
       success: (choices) =>
         if choices?.length > 0
-          cb choices
+          for choice in choices
+            urls.push { id: choice.objectId, url: choice.image }
+#          fetchImageUrls limit, skip + limit, urls, cb
+          cb urls
         else
-          cb null
+          cb urls
       error: (error) ->
         console.log "Error fetching choices: " + error.code + " " + error.message
         cb null
+
+  saveImagesToS3:  (url, filename, path, cb) ->
+
+
+
+  saveUrlToParse:  (id, full, thumb, cb) ->
+    choiceQuery = new Parse.Query 'Choice'
+    choiceQuery.equalTo 'objectId', id
+    choiceQuery.first
+      success: (result) =>
+        if result?
+          result.set 'plugFull', full
+          result.set 'plugThumb', thumb
+          result.save()
+          cb "success"
+        else
+          cb null
 
 window.migrate = new Migrate()
--- client/run/migrateS3.js
+++ client/run/migrateS3.js
@@ -3,11 +3,27 @@
 
   Parse.initialize("oBVGtJcA1YL8qAHtRHE1sm46edv3CdxSDCCBAyH3", "jOzfgxXTgD83QGnSyEcJgdCGEE4tNQz2bMEP4Zxe");
 
+  filepicker.setKey("A36NnDQaISmXZ8IOmKGEQz");
+
   Migrate = (function() {
     function Migrate() {}
 
-    Migrate.prototype.migrate = function() {
-      filepicker.setKey("A36NnDQaISmXZ8IOmKGEQz");
+    Migrate.prototype.moveImagesToS3 = function() {
+      return this.fetchImageUrls(5, 0, [], function(items) {
+        var filename, item, matches, path, _i, _len, _results;
+        _results = [];
+        for (_i = 0, _len = items.length; _i < _len; _i++) {
+          item = items[_i];
+          matches = item.url.match(/[^\/]+(#|\?|$)/);
+          filename = "_" + item.id + "." + matches[0];
+          path = '/full';
+          _results.push(console.log(item));
+        }
+        return _results;
+      });
+    };
+
+    Migrate.prototype.pickAndStore = function() {
       return filepicker.pickAndStore({
         mimetype: "image/*"
       }, {
@@ -32,18 +48,27 @@
       });
     };
 
-    Migrate.prototype.fetchImageUrls = function() {
-      var Choice, card, choiceQuery;
+    Migrate.prototype.fetchImageUrls = function(limit, skip, urls, cb) {
+      var Choice, choiceQuery;
       Choice = Parse.Object.extend('Choice');
       choiceQuery = new Parse.Query(Choice);
-      card = new Parse.Object('Card');
+      choiceQuery.limit(limit);
+      choiceQuery.skip(skip);
       return choiceQuery.find({
         success: (function(_this) {
           return function(choices) {
+            var choice, _i, _len;
             if ((choices != null ? choices.length : void 0) > 0) {
-              return cb(choices);
+              for (_i = 0, _len = choices.length; _i < _len; _i++) {
+                choice = choices[_i];
+                urls.push({
+                  id: choice.objectId,
+                  url: choice.image
+                });
+              }
+              return cb(urls);
             } else {
-              return cb(null);
+              return cb(urls);
             }
           };
         })(this),
@@ -51,6 +76,28 @@
           console.log("Error fetching choices: " + error.code + " " + error.message);
           return cb(null);
         }
+      });
+    };
+
+    Migrate.prototype.saveImagesToS3 = function(url, filename, path, cb) {};
+
+    Migrate.prototype.saveUrlToParse = function(id, full, thumb, cb) {
+      var choiceQuery;
+      choiceQuery = new Parse.Query('Choice');
+      choiceQuery.equalTo('objectId', id);
+      return choiceQuery.first({
+        success: (function(_this) {
+          return function(result) {
+            if (result != null) {
+              result.set('plugFull', full);
+              result.set('plugThumb', thumb);
+              result.save();
+              return cb("success");
+            } else {
+              return cb(null);
+            }
+          };
+        })(this)
       });
     };
 
